76d8c9c8f4d5c8dcb6b81a892823843bf17d3b26
Startica Dev <iovu1799@gmail.com>
2026-06-25 18:16:16 +0300

    feat(kindergartens): wire /kindergartens route with super_admin role gating

---
+++ src/pages/kindergartens.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import KindergartensListPage from '~/modules/kindergartens/pages/KindergartensListPage.vue'
+
+definePageMeta({ layout: 'admin', middleware: ['role'], roles: ['super_admin'] })
+</script>
+
+<template>
+  <KindergartensListPage />
+</template>
