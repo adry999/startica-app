d839aea1df6e6776e1c30530840589c75b6acb95
Startica Dev <iovu1799@gmail.com>
2026-07-05 04:20:36 +0300

    feat(pool): calendar and trainer settings pages

---
+++ src/pages/pool/settings.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import PoolTrainerSettingsPage from '~/modules/pool/pages/PoolTrainerSettingsPage.vue'
+definePageMeta({ layout: 'admin' })
+</script>
+
+<template><PoolTrainerSettingsPage /></template>
